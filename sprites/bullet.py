c864df832411848b173ba7a067cde3e7c80c0354
Yuri <yuri.vdl@gmail.com>
2016-09-21 00:43:39 -0300

    Adicionado colizao das bullets

--- sprites/bullet.py
+++ sprites/bullet.py
@@ -1,14 +1,18 @@
+import pygame.sprite
 from pygame.sprite import Sprite
 from pygame import Surface
 from gamelib.rgb import WHITEBLUE
 from math import sin
 from math import cos
 from sprites import miscs
+from sprites import enemies
+from sprites import players
 
 class Bullet(Sprite):
 	width = 5
 	height = 5
 	speed = 10
+	is_player = False
 
 	def __init__(self, spriteOrigin, angle):
 		Sprite.__init__(self, miscs)
@@ -25,7 +29,7 @@ class Bullet(Sprite):
 		self.move_y = (-1 * round(sin(angle))) * 10
 		self.move_x = (-1 * round(cos(angle))) * 10
 
-		#print(angle, round(sin(angle)), round(cos(angle)))
+		self.enemy_group = enemies if spriteOrigin in players else players
 
 	def update(self):
 		self.rect.y += self.move_y
@@ -33,12 +37,12 @@ class Bullet(Sprite):
 
 		#self.rect.y = self.rect.y-10
 		#self.image.fill(WHITEBLUE)
+		collidedWithEnemy = False
+		enemy_collide = pygame.sprite.spritecollideany(self, self.enemy_group)
 
-		#enemy_collide = pygame.sprite.spritecollideany(self,enemies_group)
 
-		#if enemy_collide:
-		#	enemy_collide.kill()
-		#	self.kill()
-
-		#if(self.rect.y < 0):
-		#	self.kill()
+		if enemy_collide:
+			enemy_collide.kill()
+			self.kill()
+		elif(self.rect.y < 0):
+			self.kill()
